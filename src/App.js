686cb49af29967c390ea64a6ef61a010e5b6129b
Marllon Wagner <marllonwagner91@gmail.com>
2022-11-09 16:34:05 -0300

    req2 criando a Home

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  // state = {  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={ Home } />
+      </Switch>
+
+    );
+  }
 }
 
 export default App;
